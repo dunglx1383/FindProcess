509db8cb0fa8fb7e5cec961c4b3347390d291ef9
Dung Le <dungle5@deloitte.com>
2020-11-04 15:33:03 +0700

    First commit

---
+++ ProcessUtils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Management;
+using System.Linq;
+
+namespace FindProcess
+{
+    class ProcessUtils
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+        public static Process GetForegroundProcess()
+        {
+            uint processId = 0;
+            IntPtr hWnd = GetForegroundWindow();
+            uint threadId = GetWindowThreadProcessId(hWnd, out processId);
+            return Process.GetProcessById(Convert.ToInt32(processId));
+        }
+
+        public static List<Proc> GetChildProcesses(int parentId)
+        {
+            // For whole class, check out this page
+            // https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-process
+            var searcher = new ManagementObjectSearcher("Select Name, ProcessId From Win32_Process Where ParentProcessId = " + parentId);
+            var processList = searcher.Get();
+            return processList.Cast<ManagementObject>().Select(p => new Proc
+            {
+                Name = p.GetPropertyValue("Name").ToString(),
+                Id = Convert.ToInt32(p.GetPropertyValue("ProcessId"))
+            }).ToList();
+        }
+    }
+    class Proc
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace FindProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Process proc = ProcessUtils.GetForegroundProcess();
+                string title = !string.IsNullOrWhiteSpace(proc.MainWindowTitle) ? proc.MainWindowTitle : proc.ProcessName;
+                string text = $"[{title}]: {proc.Id}\r\n";
+                foreach (var child in ProcessUtils.GetChildProcesses(proc.Id))
+                    text += $">{child.Name}: {child.Id}\r\n";
+                Console.Clear();
+                Console.Write(text);
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
+
